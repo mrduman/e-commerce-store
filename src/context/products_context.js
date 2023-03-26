71c9f05d98ec4ae5c2b615b81fef660396a9568a
mrduman <omer.duman@gmx.de>
2023-03-26 11:49:23 +0200

    feauted product displayed

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,6 +18,10 @@ const ProductsContext = React.createContext();
 export const ProductsProvider = ({ children }) => {
   const initialState = {
     isSidebarOpen: false,
+    products_loading: false,
+    products_error: false,
+    products: [],
+    featured_products: [],
   };
   const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -30,8 +34,14 @@ export const ProductsProvider = ({ children }) => {
   };
 
   const fetchData = async (url) => {
-    const response = await axios.get(url);
-    console.log(response);
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
 
   useEffect(() => {
