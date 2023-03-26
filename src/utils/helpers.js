6e5e15c114768437e2e4ca3667cf23cb2151b223
mrduman <omer.duman@gmx.de>
2023-03-26 17:24:14 +0200

    price formatted

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  const newNumber = Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(number / 100);
+  return newNumber;
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
